2df7dc5bf3f9bbe399bc816ecb28c6980b4031e2
rebecaachambers <rebecaachambers@users.noreply.github.com>
2026-06-10 22:27:28 +0800

    Add popup dialog: 'config complete, proxy ready' after Mihomo+proxy setup

--- app/lifecycle_windows.go
+++ app/lifecycle_windows.go
@@ -9,22 +9,17 @@ import (
 	"github.com/rebecaachambers/submill/worker"
 )
 
-// onStartup is called at application start (Windows only).
-// Kills leftover processes from previous runs.
 func (app *App) onStartup() {
 	slog.Info("=== Windows startup: cleaning residue ===")
 	worker.KillResidue()
 }
 
-// onMihomoReady is called after Mihomo has been launched and is listening.
-// Sets system proxy now that the proxy server is actually ready.
 func (app *App) onMihomoReady() {
-	time.Sleep(2 * time.Second) // give mihomo time to bind ports
+	time.Sleep(2 * time.Second)
 	worker.SetSystemProxy()
+	worker.ShowReadyDialog()
 }
 
-// onShutdown is called when the application exits (Windows only).
-// Clears system proxy, kills Mihomo, and removes runtime files.
 func (app *App) onShutdown() {
 	slog.Info("=== Windows shutdown: cleaning up ===")
 	worker.ClearSystemProxy()
---
+++ worker/notify_windows.go
@@ -0,0 +1,20 @@
+﻿//go:build windows
+
+package worker
+
+import (
+	"syscall"
+	"unsafe"
+)
+
+// ShowReadyDialog pops up a message box indicating the proxy is ready.
+func ShowReadyDialog() {
+	user32 := syscall.NewLazyDLL("user32.dll")
+	msgBox := user32.NewProc("MessageBoxW")
+	msgBox.Call(
+		0,
+		uintptr(unsafe.Pointer(syscall.StringToUTF16Ptr("SubMill\n\n配置完成，可以正常访问！"))),
+		uintptr(unsafe.Pointer(syscall.StringToUTF16Ptr("SubMill"))),
+		uintptr(0x40), // MB_ICONINFORMATION
+	)
+}
